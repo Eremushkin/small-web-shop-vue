e6c3347c39e5355fbb1efd2af77cfbea1ffad550
daniileremuskin <eremushkin64@gmail.com>
2024-02-09 13:23:00 +0700

    6

--- src/components/TgSqanQrPage.vue
+++ src/components/TgSqanQrPage.vue
@@ -2,7 +2,6 @@
   <div class="qr-scan-page">
     <h1>Scan Result</h1>
     <h2>{{scanResult.value}}</h2>
-    <label>{{params.value}}</label>
     <button @click="handleScanQR">SCAN QR</button>
   </div>
 </template>>
@@ -13,15 +12,13 @@ import {ref} from "vue";
 export default {
   setup() {
     const scanResult = ref("")
-    const  params = ref([])
-    return {scanResult, params}
+    return {scanResult}
   },
   methods: {
     handleScanQR() {
       console.log('QR code scan initiated');
       window.Telegram.WebApp.showScanQrPopup({"text": "Scan any QR"}, (data) => {
         console.log(data)
-        this.params.value = data
         this.scanResult.value = data
         return true;
       })
